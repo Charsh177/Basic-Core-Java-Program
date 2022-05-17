bf17f079e95bcae83ec7346c66af8cc6dd715459
Charsh177 <harshchougule177@gmail.com>
2022-05-17 16:36:29 +0530

    Calculate flip coin head/tail random percentage

--- src/com/corejavafunc/ha/FlipCoinPercentage.java
+++ src/com/corejavafunc/ha/FlipCoinPercentage.java
@@ -31,13 +31,15 @@ public class FlipCoinPercentage
                 System.out.println("\t tails");
             }
             counter++;
+
         }
             System.out.println();
             System.out.println("Number of Heads = " + heads);
             System.out.println("Number of Tails = " + tails);
-        {
             int percHead = (flips / heads) * 100;
             int percTail = (flips / tails) * 100;
+
+        {
             System.out.println("Percentage of Head is " + (percHead));
             System.out.println("Percentage of Tail is " + (percTail));
         }
